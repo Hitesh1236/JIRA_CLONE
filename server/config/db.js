fcf9b3a9413c079f2a4653b6c9d23f8bc11f91cd
Hitesh1236 <hitupatil1148@gmail.com>
2026-07-26 21:39:36 +0530

    making changes in mongo config

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,14 @@
 import mongoose from "mongoose";
+
 export const connectDB = async () => {
-  console.log("MONGO_URI is set:", !!process.env.MONGO_URL);
-  await mongoose.connect(process.env.MONGO_URL);
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      tls: true,
+      serverSelectionTimeoutMS: 5000, // Don't hang for 30s
+    });
+    console.log("MongoDB connected successfully!");
+  } catch (err) {
+    console.error("DB Error details:", err.message);
+    throw err;
+  }
 };
