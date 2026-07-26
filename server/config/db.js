144be1e9b2adbfadff692ced77886251960986d1
Hitesh1236 <hitupatil1148@gmail.com>
2026-07-26 21:11:20 +0530

    modified domain

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
 export const connectDB = async () => {
+  console.log("MONGO_URI is set:", !!process.env.MONGO_URL);
   await mongoose.connect(process.env.MONGO_URL);
 };
