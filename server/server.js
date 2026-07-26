ad6c3d4974cc5cbc8faaa5342f0ee5974080114b
Hitesh1236 <hitupatil1148@gmail.com>
2026-07-26 21:00:20 +0530

    modified domain

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 import dotenv from "dotenv";
 dotenv.config()
 
-import dns from "node:dns/promises"
-
-dns.setServers(["1.1.1.1", "8.8.8.8"]);
+import dns from 'node:dns';
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+dns.setDefaultResultOrder('ipv4first');
 
 import express from "express";
 import cors from 'cors'
