59b7ea348480d7bb40ff5747f271b3736284f802
Hitesh1236 <hitupatil1148@gmail.com>
2026-07-27 18:55:53 +0530

    debug logs

--- server/config/db.js
+++ server/config/db.js
@@ -1,17 +1,45 @@
-import dns from "node:dns/promises";
 import mongoose from "mongoose";
+import dns from "node:dns/promises";
+import net from "node:net";
 
 export const connectDB = async () => {
   try {
-    console.log(
-      await dns.resolveSrv("_mongodb._tcp.cluster0.rtp5lls.mongodb.net")
+    // Fix DNS issue on local Windows environment
+    dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
+    const records = await dns.resolveSrv(
+      "_mongodb._tcp.cluster0.rtp5lls.mongodb.net"
     );
+
+    console.log("MongoDB SRV records:", records);
+
+    const host = records[0].name;
+
+    const socket = net.createConnection({
+      host,
+      port: 27017,
+      timeout: 5000,
+    });
+
+    socket.on("connect", () => {
+      console.log("TCP CONNECTION SUCCESS:", host);
+      socket.destroy();
+    });
+
+    socket.on("error", (err) => {
+      console.log("TCP CONNECTION ERROR:", err.message);
+    });
+
+    socket.on("timeout", () => {
+      console.log("TCP CONNECTION TIMEOUT");
+      socket.destroy();
+    });
 
     await mongoose.connect(process.env.MONGO_URL);
 
     console.log("MongoDB connected successfully!");
   } catch (err) {
-    console.error("DB Error details:", err);
+    console.error("DB Error:", err);
     throw err;
   }
 };
