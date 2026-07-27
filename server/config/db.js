e987d21701ae49659bfbdc86f4ebff93e29a1f17
Hitesh1236 <hitupatil1148@gmail.com>
2026-07-27 19:17:32 +0530

    debug logs

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ import dns from "node:dns/promises"
 
 export const connectDB = async () => {
   try {
-
+    dns.setServers(["1.1.1.1","8.8.8.8"])
     await mongoose.connect(process.env.MONGO_URL);
     console.log("MongoDB connected successfully!");
   } catch (err) {
